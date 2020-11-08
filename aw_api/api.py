356e4c59d193ccb3301015a1793afb58bd3f4ba8
lookandhate <princessselestiapwnz@mail.ru>
2020-11-08 13:41:47 +0400

    Initial commit

---
+++ aw_api/api.py
@@ -0,0 +1,135 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+from typing import Union
+
+from .exceptions import *
+
+
+class API:
+    def __init__(self, raw_cookie: Union[dict, list] = None):
+        """
+        :param raw_cookie: dict containing exported with "EditThisCookie" Chrome extension cookie from aw.mail.ru
+        """
+
+        # Paragraph that shows if we are not authenticated on site
+        self.__NOT_AUTH_CHECK = '<p>Для просмотра данной страницы вам необходимо авторизоваться или <a href="/user/register/">зарегистрироваться</a> на сайте.</p>'
+        # Div with text shows that user closed his statistics
+        self.__CLOSED_STAT = '<div class="node_notice warn border">Пользователь закрыл доступ!</div>'
+        # base url for player statistics
+        self.__stats_url = 'https://aw.mail.ru/dynamic/user/?a=stats'
+        # Session that will contain cookies
+        self.__session: requests.Session = requests.Session()
+        # Dict with cookies
+        self.__cookie: Union[dict, list, None] = None
+        if raw_cookie:
+            self.__cookie = self.__prepare_cookie(raw_cookie)
+
+    # Отчистка HTML от тэгов
+    @staticmethod
+    def __clean_html(raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+    @staticmethod
+    def __prepare_cookie(raw_cookie: Union[dict, list]) -> dict:
+        """
+        :param raw_cookie: Raw cookie from EditThisCookie
+        :return: Dict with "cleaned" cookies
+        """
+
+        new_cookie_dict = dict()
+        for item in raw_cookie:
+            new_cookie_dict[item['name']] = item['value']
+        return new_cookie_dict
+
+    def __get_page(self, url):
+        """
+        :param url: URL to retrive
+        :return: string with decoded HTML page
+        """
+        r = self.__session.get(url, cookies=self.__cookie)
+        print(url)
+        if r.status_code == 200:
+            page = r.content.decode('utf-8')
+            return page
+        raise BadHTTPStatusCode(f'Got non 200 status code: {r.status_code}', r.status_code)
+
+    def __get_player_statistic_page(self, nickname: str, mode: int, data: int, tank_id: id, day: int = 0,
+                                    ajax: int = 0) -> str:
+        """
+        :param nickname: Nickname of user to find
+        :param mode: Game mode Number from 0 to 4 {pvp, pve, low, glops, ranked}
+        :param data: CSA ID of player to find(overwrites user nickname) if not 0
+        :param tank_id: staticID of tank to find for(0 means overall stat for mode)
+        :param day: Filter stats by some date/battle count
+        :param ajax: Is data should be returned like in ajax request (DONT CHANGE IT OR WILL BROKE)
+        :return: string with decoded HTML page
+
+        """
+
+        url = f'{self.__stats_url}&name={nickname}&mode={mode}&data={data}&type={tank_id}&day={day}&ajax={ajax}'
+        return self.__get_page(url)
+
+    def __get_stat(self, page: str, nickname=None) -> dict:
+        """
+        :param page: string with HTML document
+        :param nickname: Nickname of player to rise exception with
+
+        :return: dict with user data, where
+        winrate: float - Player win rate in percents
+        battles: int - Total player battles
+        damage: float - Player average damage
+        clantag: Optional[str] - player battalion tag (can be None)
+        nickname: str - Correct player nickname
+        """
+
+        # Парсинг страницы
+        soup = BeautifulSoup(page, "html.parser")
+        ss = list(soup.find_all('p'))
+        if not ss:
+            ss = soup.find_all('div')
+        ss = list(ss)
+
+        # Проверяем код страницы на премет строки NOT_AUTH_CHECK
+        if self.__NOT_AUTH_CHECK == str(ss[0]):
+            raise NotAuthException('__NOT_AUTH_CHECK = str(ss[0])')
+
+        # Проверяем код страницы на предмет строки уведомляющей о том,что пользователя не сушествует
+        if '<div class="node_notice warn border">Пользователь не найден!</div>' == str(ss[0]):
+            raise UserNotFoundException('User with given nickname was not found')
+
+        # Check did user closed stats
+        if '<div class="node_notice warn border">Пользователь закрыл доступ!</div>' == str(ss[0]):
+            raise UserHasClosedStatisticsException('User with given nickname closed his stats')
+
+        nickname = self.__clean_html(str(soup.find("div", {"class": "name"}))).split('\n')[1]
+        battalion = soup.find('div', {'class': 'clan'})
+        battalion = str(battalion.contents[3]).split()
+        battalion = battalion[0].replace('<span>', '').replace('[', '').replace(']', '')
+        if len(battalion) == 0:
+            battalion = None
+
+        battle_stats = str(soup.find("div", {"class": "total"}))
+        battle_stats = int(self.__clean_html(battle_stats).split()[-1].replace('сыграно', ''))
+
+        avg_dmg = soup.findAll("div", {"class": "list_pad"})
+        clear = self.__clean_html(str(avg_dmg[3]))
+        avg_dmg = clear.split('\n')
+        avg_dmg = avg_dmg[4]
+        avg_dmg = avg_dmg.strip()[3::]
+
+        winrate_stat = str(soup.find("span", {"class": "yellow"}))
+        winrate_stat = self.__clean_html(winrate_stat)
+
+        return {'winrate': float(winrate_stat[:-1]), 'battles': battle_stats,
+                'damage': float(avg_dmg), 'clantag': battalion, 'nickname': nickname}
+
+    def get_statistic_by_nickname(self, nickname, mode=0, data=0, type_tank=0, day=0):
+        # Get page
+        page = self.__get_player_statistic_page(nickname, mode, data, type_tank, day)
+        # print(page)
+        # Parse the page
+        parsed_data = self.__get_stat(page, nickname)
+        return parsed_data
---
+++ aw_api/exceptions.py
@@ -0,0 +1,26 @@
+class BaseStatsServiceException(Exception):
+    """base exception for statistics parser"""
+
+
+class UserNotFoundException(BaseStatsServiceException):
+    """Raises whenever user with given nickname was not found"""
+
+    def __init__(self, nickname, *args, **kwargs):
+        self.nickname = nickname
+        super().__init__(*args, **kwargs)
+
+
+class UserHasClosedStatisticsException(BaseStatsServiceException):
+    """Raises when requested user has closed stats"""
+    pass
+
+
+class NotAuthException(BaseStatsServiceException):
+    """Raises if bot did not pass auth"""
+    pass
+
+
+class BadHTTPStatusCode(BaseStatsServiceException):
+    def __init__(self, status_code, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.status_code = status_code
