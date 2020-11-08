cc9ba39c2522735893c40f5ed0d3ae0960924550
lookandhate <princessselestiapwnz@mail.ru>
2020-11-08 16:47:58 +0400

    A little more refactoring

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aw_api',
+    version='0.1.0',
+    packages=['aw_api'],
+    url='https://github.com/lookandhate/ArmoredWarfareAPI',
+    license='MIT',
+    author='https://github.com/lookandhate',
+    author_email='',
+    description='Armored Warfare statistics API'
+)
