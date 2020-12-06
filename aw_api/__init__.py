610ea76c5cc16bac57c5dd718b14260326a966a6
lookandhate <princessselestiapwnz@mail.ru>
2020-12-06 15:49:36 +0400

    Added License in source code files

--- aw_api/__init__.py
+++ aw_api/__init__.py
@@ -1,2 +1,13 @@
+"""
+Armored Warfare API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+Parser simulating API for ArmoredWarfare players statistics.
+
+:copyright: (c) 2020 lookandhate
+:license: MIT, see LICENSE for more details.
+
+"""
+
 from .api import API, AW
 import aw_api.exceptions
