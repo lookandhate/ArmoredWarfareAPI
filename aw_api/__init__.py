f5ea54aa2dcc5eed839b115f08eab7d1fac51ff7
lookandhate <princessselestiapwnz@mail.ru>
2020-12-06 14:44:27 +0400

    Added battalion not found exception in __init__.py

--- aw_api/__init__.py
+++ aw_api/__init__.py
@@ -1,3 +1,3 @@
 from .api import API
-from .exceptions import UserHasClosedStatisticsException, UserNotFoundException, BadHTTPStatusCode, NotAuthException
-
+from .exceptions import UserHasClosedStatisticsException, UserNotFoundException, BadHTTPStatusCode, NotAuthException, \
+    BattalionNotFound
