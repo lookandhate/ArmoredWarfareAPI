ed16cb74930debce7a90404f708fe83f327be904
lookandhate <princessselestiapwnz@mail.ru>
2020-12-18 20:34:08 +0400

    Added new checks to player statistic tests

--- module_tests/test_aw_api.py
+++ module_tests/test_aw_api.py
@@ -14,7 +14,7 @@ test_data = {
 }
 
 
-def test_general():
+def test_pvp():
     player_data = client.get_statistic_by_nickname('IterasuGr1njo', mode=0)
 
     assert player_data['winrate'] == 65.6
@@ -22,6 +22,8 @@ def test_general():
     assert player_data['damage'] == 6815.85
     assert player_data['clantag'] is None
     assert player_data['nickname'] == 'IterasuGr1njo'
+    assert player_data['average_spotting'] == 613.4325153374233
+    assert player_data['average_kills'] == 2.25
 
 
 def test_pve():
@@ -32,6 +34,8 @@ def test_pve():
     assert player_data['damage'] == 9465.28
     assert player_data['clantag'] is None
     assert player_data['nickname'] == 'IterasuGr1njo'
+    assert player_data['average_spotting'] == 1923.128205128205
+    assert player_data['average_kills'] == 3.4
 
 
 def test_low():
@@ -41,6 +45,8 @@ def test_low():
     assert other_player['winrate'] == 87.5
     assert other_player['battles'] == 48
     assert other_player['damage'] == 2598.97
+    assert other_player['average_spotting'] == 900.875
+    assert other_player['average_kills'] == 1.19
 
     assert player_data['winrate'] == 0
     assert player_data['battles'] == 0
@@ -57,8 +63,13 @@ def test_glops():
     assert player_data['damage'] == 15336.55
     assert player_data['clantag'] is None
     assert player_data['nickname'] == 'IterasuGr1njo'
+    assert int(player_data['average_spotting']) == 2352
+    assert player_data['average_kills'] == 4.79
 
 
-def test_battalion():
+def disabled_test_battalion(): # This test has been disabled because battalion with id = 348473 does not exist anymore
     data = client.get_battalion_players(348473)
     assert data == test_data['battalion_players_348473']
+
+
+test_pvp()
