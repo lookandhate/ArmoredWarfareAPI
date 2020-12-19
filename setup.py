beb54d4fbd297d1ae25029958db9fb9a5555a283
lookandhate <princessselestiapwnz@mail.ru>
2020-12-19 21:26:57 +0400

    Updated description in setup

--- setup.py
+++ setup.py
@@ -8,11 +8,20 @@ setup(
     name='aw_api',
     version='0.5.0',
     packages=['aw_api'],
+    description='Armored Warfare unofficial API',
     url='https://github.com/lookandhate/ArmoredWarfareAPI',
     license='MIT',
     author='https://github.com/lookandhate',
     author_email='',
-    description='Armored Warfare statistics API',
     install_requires=requirements,
-    python_requires='>=3.6.0'
+    python_requires='>=3.6.0',
+    keywords=['armored warfare', 'aw', 'armored warfare api', 'armata', 'армата'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
 )
