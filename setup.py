66c1d59e306071e17f59e64d0b1cf76e8d7b38dc
lookandhate <princessselestiapwnz@mail.ru>
2020-12-20 20:01:27 +0400

    Upgraded minimum python version from 3.6 up to 3.7

--- setup.py
+++ setup.py
@@ -12,11 +12,10 @@ setup(
     author='https://github.com/lookandhate',
     author_email='',
     install_requires=['requests', 'beautifulsoup4'],
-    python_requires='>=3.6.0',
+    python_requires='>=3.7.0',
     keywords=['armored warfare', 'aw', 'armored warfare api', 'armata', 'армата'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
